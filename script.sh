ea0e365be371504137f4aeb530d66e1ca805647c
Kushan Thamel <kushan.thamel@gmail.com>
2024-01-15 09:03:41 +0000

    removed the efs configurations

--- script.sh
+++ script.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "## Attach the Jenkins Data Volume ##"
-sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.jenkins-data.dns_name}:/  /var/lib/jenkins
-echo ${aws_efs_file_system.jenkins-data.dns_name}:/ /var/lib/jenkins nfs4 defaults,_netdev 0 0  | sudo cat >> /etc/fstab
-
 echo "## Install Jenkins on Amazon Linux ##"
 sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
