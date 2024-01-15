69da08fbc7f05170206e5a80e286f4040db39974
Kushan Thamel <kushan.thamel@gmail.com>
2024-01-15 08:33:53 +0000

    modified the script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 echo "## Attach the Jenkins Data Volume ##"
-sudo mkfs.ext4 -F /dev/xvdk
-sudo mkdir -p /var/lib/jenkins
-sudo mount /dev/xvdk /var/lib/jenkins
-BLK_ID=$(sudo blkid /dev/xvdk | cut -f2 -d" ")
-echo "$BLK_ID     var/lib/jenkins   ext4    defaults   0   2" | sudo tee --append /etc/fstab
-sudo mount -a
+sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.jenkins-data.dns_name}:/  /var/lib/jenkins
+echo ${aws_efs_file_system.jenkins-data.dns_name}:/ /var/lib/jenkins nfs4 defaults,_netdev 0 0  | sudo cat >> /etc/fstab
 
 echo "## Install Jenkins on Amazon Linux ##"
 sudo yum update -y
